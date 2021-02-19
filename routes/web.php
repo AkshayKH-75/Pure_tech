5955dd47c2bbc1a6e99e02f8b551232b880df958
AkshayKH-75 <68735643+AkshayKH-75@users.noreply.github.com>
2021-02-19 11:01:24 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::get("/tabel","Tabel@index")->name('tabel');
-// Route::get("/datatabel","Tabel@dataTabelindex")->name('datatabel');
 
 
 Route::get("/home","DigitToWord@DigitToWord")->name('home');
